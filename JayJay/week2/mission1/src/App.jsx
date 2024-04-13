c6016f9323991b97dc7677afd4d4576cb69764a0
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-04-13 11:05:32 +0900

    코드 주석설명 추가

--- JayJay/week2/mission1/src/App.jsx
+++ JayJay/week2/mission1/src/App.jsx
@@ -13,15 +13,16 @@ function App() {
   const addTodo = (newTodo) => {
     const newId = todos.length > 0 ? todos[todos.length - 1].id + 1 : 1;
     setTodos([...todos, { id: newId, content: newTodo, isDone: false }]);
+    //해야할 일에는 isDone이 false 일것
   };
 
   const completeTodo = (id) => {
     const updatedTodos = todos.map((todo) => {
       if (todo.id === id) {
-        //...todo가 의미하는 것은 뭘까??
+        //배열 안에 있는 객체에 각 요소들을 spread 연산자로 이용
         return { ...todo, isDone: true };
       }
-      return todo;
+      return todo; //같은 id가 없는 경우 todo 그대로 리턴, 즉 todos배열 그대로 유지
     });
     setTodos(updatedTodos);
     setCompletedTodos([...updatedTodos.filter((todo) => todo.isDone === true)]);
