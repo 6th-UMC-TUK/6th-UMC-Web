8e856e6603436960ad71224e5427fbe6e613176d
choihoo <hochoi8621@gmail.com>
2024-04-30 15:44:19 +0900

    week2 mission2 css 수정

--- Rice/week2/mission2/src/component/Movie.jsx
+++ Rice/week2/mission2/src/component/Movie.jsx
@@ -3,22 +3,20 @@ import { useState } from "react";
 import MovieDetail from "./MovieDetail";
 
 const IMG_BASE_URL = "https://image.tmdb.org/t/p/w1280/";
-// TODO: 마우스 올리면 내용 나오는 거 수정
+
 const Movie = ({ movie }) => {
   const [isHover, setisHover] = useState(false);
 
   return (
     <div
-      className="movie-hover"
+      className={`movie-container ${isHover ? "hovered" : ""}`}
       onMouseOver={() => setisHover(true)}
       onMouseOut={() => setisHover(false)}
     >
-      <div className={`movie-container ${isHover ? "hovered" : ""}`}>
-        <img src={IMG_BASE_URL + movie.poster_path} alt="영화포스터" />
-        <div className="movie-info">
-          <h4>{movie.title}</h4>
-          <span>{movie.vote_average}</span>
-        </div>
+      <img src={IMG_BASE_URL + movie.poster_path} alt="영화포스터" />
+      <div className="movie-info">
+        <h4>{movie.title}</h4>
+        <span>{movie.vote_average}</span>
       </div>
       {isHover && <MovieDetail title={movie.title} overview={movie.overview} />}
     </div>
