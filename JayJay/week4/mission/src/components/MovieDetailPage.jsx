ad5a35552bde5492c56e6fca4cb95f61cc6f1d2c
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-05-04 11:43:44 +0900

    feat: 영화 상세페이지 배경화면 구현 완료

--- JayJay/week4/mission/src/components/MovieDetailPage.jsx
+++ JayJay/week4/mission/src/components/MovieDetailPage.jsx
@@ -9,13 +9,17 @@ const MovieDetailBox = styled.div`
   align-items: center;
   width: 100%;
   min-height: 100vh; // 뷰포트의 높이를 기준으로 100%
+  background-image: url(${(props) => props.backdrop});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
 `;
 
 const MovieDetailInfoBox = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: space-evenly;
   align-items: center;
-  width: 600px;
+  width: 100%;
   max-height: 100%;
   color: white;
 `;
@@ -25,7 +29,8 @@ const MovieDetailInfo = styled.div`
   flex-direction: column;
   align-items: start;
   justify-content: center;
-  width: 300px;
+  width: 400px;
+  min-width: 400px;
 `;
 
 const MovieDetailImage = styled.img`
@@ -37,20 +42,30 @@ export default function MovieDetailPage() {
   const location = useLocation();
   const { movie } = location.state || {}; // location.state에서 movie 데이터를 추출
 
+  const backdropUrl = movie.backdrop_path
+    ? `https://image.tmdb.org/t/p/original${movie.backdrop_path}`
+    : "";
+
   if (!movie) {
     return <Loading />;
   }
 
   return (
-    <MovieDetailBox>
+    <MovieDetailBox backdrop={backdropUrl}>
       <MovieDetailInfoBox>
         <MovieDetailImage
           src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
           alt={movie.title}
         />
         <MovieDetailInfo>
-          <h1>{movie.title}</h1>
-          <p>{movie.release_date}</p>
+          <div>{movie.title}</div>
+          <div>{movie.vote_average}</div>
+          <h1>{movie.release_date}</h1>
+          {movie.overview ? (
+            <p>{movie.overview}</p>
+          ) : (
+            <p>TDMB에서 제공하는 상세정보가 없습니다.</p>
+          )}
           <p>{movie.overview}</p>
         </MovieDetailInfo>
       </MovieDetailInfoBox>
