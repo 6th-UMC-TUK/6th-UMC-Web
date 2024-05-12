0867a31034c5f6ecd332772917e5d980269f3b68
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-05-12 15:13:19 +0900

    chore: 컴포넌트명 오류 수정

--- JayJay/week5/mission/src/components/SignUpPage.jsx
+++ JayJay/week5/mission/src/components/SignUpPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import styled from "styled-components";
-import InputField from "./InputFiled";
+import InputField from "./InputField";
 import { useNavigate } from "react-router-dom";
 
 const SignUpPageWrapper = styled.div`
